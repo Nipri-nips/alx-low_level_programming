57dc5f3d2463ead6b9c7bba504e309f7a05b5fb3
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-09-09 18:57:34 -0700

    101 and 102

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - This code prints all possible combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		write(2,
-				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
-}
+	int n, m, l;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
