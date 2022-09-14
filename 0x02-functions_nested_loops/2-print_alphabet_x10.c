c0b55428d3a8e18abc460e9c2634f5a5cd1e4557
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-09-13 17:19:26 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char e, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; e <= 'z'; e++)
+		{
+			_putchar(e);
+		}
+		_putchar('\n');
+	}
+}
