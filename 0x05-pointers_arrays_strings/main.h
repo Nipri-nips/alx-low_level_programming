cacf2d3632fccff87253cf56a0d017336d1b8f7b
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-09-19 17:06:54 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,9 @@
 #define MAIN_H
 
 /**
-* void reset_to_98(int *n); - void prototypes()
+* void prototypes()
 * int prototypes()
-*
-*/)
+*/
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
