187aedc1d37fc9aa7a19ab54f84e41f753a3dd89
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-09-22 16:14:16 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,30 +1,13 @@
-#include "main.h"
-
-void print_uint(unsigned int n);
-
-/**
-* print_number - Prints an integer
-* @n: The integer to print
-*/
-
-void print_number(int n)
-{
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-print_uint((unsigned int) n);
-}
-
-/**
-* print_uint - Prints an unsigned integer
-* @n: The unsigned int to print
-*/
-
-void print_uint(unsigned int n)
-{
-if (n / 10 != 0)
-print_uint(n / 10);
-_putchar(n % 10 + '0');
-}
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
