14d753fdf5651301801d48e8e6f20ba718ccc900
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-09-28 14:24:11 -0700

    reduced the lines on 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - this function prints a string, and it is followe ny a new line.
+ * _puts_recursion - this function prints a string, followed by a new line.
  * @s: points at the address of the string to be printed
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
