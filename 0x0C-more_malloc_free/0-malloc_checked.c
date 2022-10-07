864d37c8ca33e568d1851c12890efe0afb4d7e0a
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-10-06 18:17:56 -0700

    untracked files

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,13 @@
-#inlcude "main.h"
+#include "main.h"
 #include <stdlib.h>
+
 
 /**
  * malloc_checked - a function that allocates memory using malloc.
  * @b: input
  * Return: 0
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *memall;
