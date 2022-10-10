0c46adadc958bdb06abca94bce334e44fcfdd778
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-10-10 05:59:39 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,17 @@
  *file: dog.h
  *Desc: Header file containing all the prototypes for all functions
  */
-typeof struct dog
+typedef struct dog
 {
-name, type = char *
-age, type = float
-owner, type = char *
-}
+char *name;
+float age;
+char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 
 #endif
