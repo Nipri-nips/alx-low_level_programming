81a20ba39ebd4811457cd6409fb7346869d25b06
Nipri-nips <timinipri.ogionwo@gmail.com>
2022-10-12 16:01:50 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - prints a name
+ *@name: the name to be printed
+ *@f: the function that will be called
+ *
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
